87f3948c66c1d03ff341e00d9d37a32b401de938
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-21 21:31:19 +0200

    Project setup

---
+++ Hugo's Twitter/Core/Feed/View/FeedView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedView.swift
+//  Hugo's Twitter
+//
+//  Created by Hugo on 21.11.22.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
