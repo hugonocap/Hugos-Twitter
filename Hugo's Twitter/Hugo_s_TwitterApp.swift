0f3bd5e69176a32909c8045b030c77ebe162b17c
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-21 21:20:12 +0200

    Initial Commit

---
+++ Hugo's Twitter/Hugo_s_TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hugo_s_TwitterApp.swift
+//  Hugo's Twitter
+//
+//  Created by Hugo on 21.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Hugo_s_TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
