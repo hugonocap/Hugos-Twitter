1b4ee07bca86e9cb229da68cd4d3015e5c80e634
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-24 11:07:45 +0200

    main tab setup

---
+++ Hugo's Twitter/Core/Messages/MessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  MessagesView.swift
+//  Hugo's Twitter
+//
+//  Created by Hugo on 24.11.22.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesView()
+    }
+}
