ebb3436b0141e69d0bbeebc467a48dcb3e801810
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-26 02:27:18 +0200

    side menu view model

---
+++ Hugo's Twitter/Core/SideMenu/ViewModel/SideMenuViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  SideMenuViewModel.swift
+//  Hugo's Twitter
+//
+//  Created by Hugo on 26.11.22.
+//
+
+import Foundation
+
+enum SideMenuViewModel: Int, CaseIterable {
+    case profile
+    case lists
+    case bookmarks
+    case logout
+    
+    var description: String {
+        switch self {
+        case .profile: return "Profile"
+        case .lists: return "Lists"
+        case .bookmarks: return "Bookmarks"
+        case .logout: return "Logout"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .profile: return "person"
+        case .lists: return "list.bullet"
+        case .bookmarks: return "bookmark"
+        case .logout: return "arrow.left.square"
+        }
+    }
+}
---
+++ Hugo's Twitter/Core/SideMenu/Views/SideMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  SideMenuView.swift
+//  Hugo's Twitter
+//
+//  Created by Hugo on 26.11.22.
+//
+
+import SwiftUI
+
+struct SideMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SideMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuView()
+    }
+}
