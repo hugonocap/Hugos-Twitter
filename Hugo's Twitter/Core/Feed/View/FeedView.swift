1a7fdd6c7de4b27370915ee8766c8925cb9eb4e3
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-26 12:22:49 +0200

    floating button

--- Hugo's Twitter/Core/Feed/View/FeedView.swift
+++ Hugo's Twitter/Core/Feed/View/FeedView.swift
@@ -9,12 +9,29 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(0 ..< 20, id: \.self) { _ in
-                    TweetRowView()
+        ZStack(alignment: .bottomTrailing) {
+            ScrollView {
+                LazyVStack {
+                    ForEach(0 ..< 20, id: \.self) { _ in
+                        TweetRowView()
+                    }
                 }
             }
+            
+            Button {
+                //
+            } label: {
+                Image(systemName: "pencil")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 28, height: 28)
+                    .padding()
+            }
+            .background(Color(.systemBlue))
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .padding()
+            
         }
     }
 }
