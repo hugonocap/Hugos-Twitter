59c06d27576c58c331ca6e7de3705a6db8bf32bf
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-26 03:13:40 +0200

    side menu

--- Hugo's Twitter/ContentView.swift
+++ Hugo's Twitter/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showMenu: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack(alignment: .topLeading) {
+                MainTabView()
+                    .navigationBarHidden(showMenu)
+                
+                // shadow
+                if showMenu {
+                    ZStack {
+                        Color(.black)
+                            .opacity(showMenu ? 0.25 : 0.0)
+                    }
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {
+                            showMenu = false
+                        }
+                    }
+                    .ignoresSafeArea()
+                }
+                
+                SideMenuView()
+                    .frame(width: 300)
+                    .offset(x: showMenu ? 0 : -300, y: 0)
+                    .background(showMenu ? .white : .clear)
+            }
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        withAnimation(.easeInOut) {
+                            showMenu.toggle()
+                        }
+                    } label: {
+                        Circle()
+                            .frame(width: 32, height: 32)
+                    }
+
+                }
+            }
         }
-        .padding()
+        
     }
 }
 
--- Hugo's Twitter/Hugo_s_TwitterApp.swift
+++ Hugo's Twitter/Hugo_s_TwitterApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct Hugo_s_TwitterApp: App {
     var body: some Scene {
         WindowGroup {
-            MainTabView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
