ee144d631852f82f63055cf7953bec5df00522be
Hugo Montana <benzlbenny2k@gmail.com>
2022-12-02 23:08:44 +0200

    filter users

--- Hugo's Twitter/Core/Explore/Views/ExploreView.swift
+++ Hugo's Twitter/Core/Explore/Views/ExploreView.swift
@@ -9,16 +9,14 @@ import SwiftUI
 
 struct ExploreView: View {
     @ObservedObject var viewModel = ExploreViewModel()
-    @State private var searchingText = ""
-    
     var body: some View {
         VStack {
-            SearchBar(text: $searchingText)
+            SearchBar(text: $viewModel.searchText)
                 .padding()
             
             ScrollView {
                 LazyVStack {
-                    ForEach(viewModel.users) { user in
+                    ForEach(viewModel.searchableUsers) { user in
                         NavigationLink {
                                 ProfileView(user: user)
                         } label: {
